b787ad776e5fe84b6b2825cadfcd6d8cac1e4d72
Facundo Arebalo <facuar1589@gmail.com>
2024-05-12 16:58:58 -0300

    view detalles terminada

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { format } from "date-fns"
+import { es } from "date-fns/locale"
 import styles from './Detail.module.css'
+
+
+
 const Detail = () => {
   const { eventId } = useParams()
   const [eventData, setEventData] = useState({});
@@ -10,26 +15,45 @@ const Detail = () => {
   useEffect(() => {
     const fetchEventData = async () => {
       try {
-        const response = fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=ZBOzG9jZAHHSllHXeI72pRLkaGCeK2AN`)
+        const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=ZBOzG9jZAHHSllHXeI72pRLkaGCeK2AN`)
         const data = await response.json()
         setEventData(data)
+
       } catch (error) {
         setEventData({})
-        SpeechSynthesisErrorEvent(error)
+        setError(error)
+        setIsLoading(false)
 
       }
     }
     fetchEventData()
   }, [])
 
-  
+  if (isLoading && Object.keys(eventData) === 0) {
+    return <h1>Cargando evento...</h1>
+  }
+  if (Object.keys(error) > 0) {
+    return <div>Error</div>
 
+  }
   return (
-    <div className={styles.container}>
-   <div className={styles.mainInfoContainer}>
-    <img src={eventData.images[0].url} alt="image" />
 
-   </div>
+    <div className={styles.container}>
+      <div className={styles.mainInfoContainer}>
+        <img src={eventData.images?.[0].url} className={styles.eventImage} alt={eventData.name} />
+        <h4 className={styles.eventName}>{eventData.name}</h4>
+        <p className={styles.eventInfo}>{eventData.info}</p>
+        {eventData.dates?.start.dateTime ? <p className={styles.dateInfo}>{format(new Date(eventData.dates.start.localDate), 'd LLLL yyyy H:mm', { locale: es })}hrs</p> : null}
+      </div>
+      <div className={styles.seatInfoContainer}>
+        <h6 className={styles.seatMapTitle}>Mapa del evento</h6>
+        <img src={eventData.seatmap?.staticUrl} alt="Seatmap Event" />
+        <p className={styles.pleaseNote}>{eventData.pleaseNote}</p>
+        <p className={styles.priceRanges}>Rango de precios: {eventData.priceRanges?.[0].min}-{eventData.priceRanges?.[0].max} $</p>
+      </div>
+      <a href={eventData.url} target="_blank">
+        <button className={styles.buyTicketButton}>Comprar boleto</button>
+      </a>
     </div>
   )
 }
